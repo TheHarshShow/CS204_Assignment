05a8ad4c0f4c95312a4ab87f53e91a11d71b8e44
Harsh Motwani <harsh18@iitg.ac.in>
2019-11-13 13:01:01 +0530

    test commit

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -12,45 +12,62 @@ using namespace std;
 
 int main(){
 
-	graph g1(10, 4);
+	// graph g1(10, 4);
 
-	g1.addEdge(2, 3);
-	g1.addEdge(1, 3, 5);
-	g1.addEdge(3, 5);
-	g1.addEdge(1, 2, 10);
+	// g1.addEdge(2, 3);
+	// g1.addEdge(1, 3, 5);
+	// g1.addEdge(3, 5);
+	// g1.addEdge(1, 2, 10);
 
-	g1.addEdge(3, 4);
-	g1.addEdge(1, 6);
-	g1.addEdge(7,8,100);
-
-
-	g1.floydWarshall();
-
-	cout<<endl;
-
-	REP(i,1,11){
-
-		g1.dijkstraDistance(i);
+	// g1.addEdge(3, 4);
+	// g1.addEdge(1, 6);
+	// g1.addEdge(7,8,100);
 
 
-	}
+	// g1.floydWarshall();
 
+	// cout<<endl;
 
-	// ll n,m;
-	// cin>>n>>m;
+	// REP(i,1,11){
 
-	// graph g1(n,m);
+	// 	g1.dijkstraDistance(i);
 
-	// REP(i,0,m){
-
-	// 	ll a,b,c;
-	// 	cin>>a>>b>>c;
-	// 	g1.addEdge(a,b,c);
 
 	// }
 
-	// cout<<g1.primMST()<<endl;
-	// cout<<g1.kruskalMST()<<endl;
+
+	// cout<<endl;
+
+	// REP(i,1,11){
+
+	// 	g1.belmanFordDistance(i);
+
+
+	// }
+
+
+	ll n,m;
+	cin>>n>>m;
+
+	graph g1(n,m);
+
+	REP(i,0,m){
+
+		ll a,b,c;
+		cin>>a>>b>>c;
+		g1.addEdge(a,b,c);
+
+	}
+
+	// g1.dfsPrint(1000);
+
+	// cout<<"dsufbashdfbzjsdhfdashbafhbsdfiusdifhsdziuf"<<endl;
+
+	// g1.bfsPrint(1000);
+
+	cout<<g1.primMST()<<endl;
+
+	cout<<g1.kruskalMST()<<endl;
 
 
 
